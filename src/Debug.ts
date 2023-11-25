931c3485bc04138cda6ccdbc459e2d1407fd84c7
Patrick Canfield <pscale01@gmail.com>
2023-11-24 16:55:57 -0800

    Update Debug.ts

--- src/Debug.ts
+++ src/Debug.ts
@@ -1,6 +1,10 @@
-export function getStackTrace() {
+export function getStackTrace(omitLines = 2) {
   const stack = new Error().stack;
   return stack !== undefined
-    ? `\n${stack.split("\n").slice(2).join("\n")}`
+    ? `\n${stack.split("\n").slice(omitLines).join("\n")}`
     : "(No stack trace)";
+}
+
+export function logWithStackTrace(...args: any[]) {
+  console.log("", ...args, getStackTrace(3));
 }
