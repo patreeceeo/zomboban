34ae4c983b3bf39669faf273c06add57e0e5bada
Patrick Canfield <pscale01@gmail.com>
2023-11-30 15:31:38 -0800

    shuffle test execution

--- test.ts
+++ test.ts
@@ -1,7 +1,13 @@
 import { sync } from "glob";
+import _ from "lodash";
 
-const files = sync("./src/**/*.test.ts");
+const files = _.shuffle(sync("./src/**/*.test.ts")).filter(
+  (file) => !file.endsWith("Component.test.ts"),
+);
+// even with the await, the tests seem to run in parallel a bit. This seems to be the only test impacted by that so run it first.
+files.unshift("./src/Component.test.ts");
 for (const file of files) {
   console.log(`Running ${file}`);
   await import(`./${file}`);
+  console.log(`Finished ${file}`);
 }
