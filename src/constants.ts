882f9d930bb789a00e7dffb7c3826e876f478a03
Patrick Canfield <pscale01@gmail.com>
2023-12-09 20:30:37 -0800

    add potion image

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,7 @@ export const NAMED_ENTITY_IMAGES: Partial<Record<EntityName, string>> = {
   [EntityName.WALL_IMAGE]: "assets/images/wall.gif",
   [EntityName.CRATE_IMAGE]: "assets/images/crate.gif",
   [EntityName.PLAYER_DOWN_IMAGE]: "assets/images/player_down.gif",
+  [EntityName.POTION_IMAGE]: "assets/images/potion.gif",
   [EntityName.EDITOR_NORMAL_CURSOR_IMAGE]: "assets/images/normal_cursor.gif",
   [EntityName.EDITOR_REPLACE_CURSOR_IMAGE]: "assets/images/replace_cursor.gif",
   [EntityName.EDITOR_ORIENT_CURSOR_IMAGE]: "assets/images/orient_cursor.gif",
