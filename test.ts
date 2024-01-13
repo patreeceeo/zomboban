de29b03b7392afb6422ed5cb50f5cf6f30f95614
Patrick Canfield <pscale01@gmail.com>
2024-01-13 11:58:08 -0800

    clean up test output

--- test.ts
+++ test.ts
@@ -7,7 +7,5 @@ const files = _.shuffle(sync("./src/**/*.test.ts")).filter(
 // even with the await, the tests seem to run in parallel a bit. This seems to be the only test impacted by that so run it first.
 files.unshift("./src/ComponentData.test.ts");
 for (const file of files) {
-  console.log(`Running ${file}`);
   await import(`./${file}`);
-  console.log(`Finished ${file}`);
 }
