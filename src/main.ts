0eae33b9854bf72836d82e483ae203cb8c17a380
Patrick Canfield <pscale01@gmail.com>
2024-01-13 17:02:44 -0800

    HMRify main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import { startApp, startLoading } from "./App";
 import { afterDOMContentLoaded } from "./util";
 
-afterDOMContentLoaded(() => {
-  startLoading(document.getElementById("game")!);
-  startApp();
-});
+if (import.meta.hot) {
+  import.meta.hot.on("vite:error", (err) => {
+    console.error(err);
+  });
+  import.meta.hot.dispose(() => {
+    import.meta.hot!.data.loaded = true;
+  });
+  import.meta.hot.accept(() => {});
+  if (!import.meta.hot!.data.loaded) {
+    afterDOMContentLoaded(() => {
+      startLoading(document.getElementById("game")!);
+      startApp();
+    });
+  }
+} else {
+  afterDOMContentLoaded(() => {
+    startLoading(document.getElementById("game")!);
+    startApp();
+  });
+}
