95e8b75f6ded701e292c0cfebc4787fe6a18d031
Patrick Canfield <pscale01@gmail.com>
2024-01-17 15:56:14 -0800

    sprite is actually a wrapper around display container

--- src/components/Sprite.ts
+++ src/components/Sprite.ts
@@ -1,41 +1,35 @@
 import { invariant } from "../Error";
-import { setRenderStateDirty } from "../systems/RenderSystem";
-import { defineComponent } from "../Component";
 import { ColorSource, Container, Texture } from "pixi.js";
-
-// TODO maybe this should just be a wrapper around DisplayContainers?
+import {
+  getDisplayContainer,
+  hasDisplayContainer,
+  removeDisplayContainer,
+  setDisplayContainer,
+} from "./DisplayContainer";
 
 type Sprite = Container & { texture: Texture; tint: number | ColorSource };
 
-const DATA = defineComponent(
-  "Sprite",
-  [],
-  hasSprite,
-  getSprite,
-  setSprite,
-  removeSprite,
-);
 export const SPRITE_SIZE = [64, 92];
 
 export function setSprite(entityId: number, value: Sprite) {
-  if (DATA[entityId] !== value) {
-    setRenderStateDirty();
-    DATA[entityId] = value;
-  }
+  setDisplayContainer(entityId, value);
 }
 
 export function hasSprite(entityId: number): boolean {
-  return DATA[entityId] !== undefined;
+  if (hasDisplayContainer(entityId)) {
+    const container = getDisplayContainer(entityId) as Sprite;
+    return container.texture !== undefined && container.tint !== undefined;
+  }
+  return false;
 }
 
 export function getSprite(entityId: number): Sprite {
   invariant(hasSprite(entityId), `Entity ${entityId} does not have a Sprite`);
-  return DATA[entityId];
+  return getDisplayContainer(entityId) as Sprite;
 }
 
 export function removeSprite(entityId: number): void {
   if (hasSprite(entityId)) {
-    setRenderStateDirty();
-    delete DATA[entityId];
+    removeDisplayContainer(entityId);
   }
 }
