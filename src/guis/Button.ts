5dcac3ef02bbf2ff3fee963aa31d2f18d9b697eb
Patrick Canfield <pscale01@gmail.com>
2024-01-18 14:57:21 -0800

    clean up

--- src/guis/Button.ts
+++ src/guis/Button.ts
@@ -1,8 +1,9 @@
 import { NineSlicePlane } from "@pixi/mesh-extras";
 import { Text } from "@pixi/text";
 import { Texture, ITextStyle } from "pixi.js";
+import { ButtonContainer } from "@pixi/ui";
 
-interface PixiButtonSettings {
+interface ButtonStyleSettings {
   width: number;
   height: number;
   padding: number;
@@ -10,7 +11,7 @@ interface PixiButtonSettings {
   labelStyle: Partial<ITextStyle>;
 }
 
-const defaultSettings: PixiButtonSettings = {
+const defaultStyle: ButtonStyleSettings = {
   width: 200,
   height: 100,
   padding: 8,
@@ -23,11 +24,17 @@ const defaultSettings: PixiButtonSettings = {
   },
 };
 
-export class PixiButton extends NineSlicePlane {
-  settings = defaultSettings;
+export class Button extends ButtonContainer {
+  constructor(public style: ButtonStyle) {
+    super(style);
+  }
+}
+
+export class ButtonStyle extends NineSlicePlane {
+  settings = defaultStyle;
   label: Text;
 
-  constructor(inputSettings?: Partial<PixiButtonSettings>) {
+  constructor(inputSettings?: Partial<ButtonStyleSettings>) {
     super(Texture.EMPTY, 0, 0, 0, 0);
 
     const label = (this.label = new Text(""));
@@ -37,7 +44,7 @@ export class PixiButton extends NineSlicePlane {
     this.update(inputSettings);
   }
 
-  update(inputSettings?: Partial<PixiButtonSettings>) {
+  update(inputSettings?: Partial<ButtonStyleSettings>) {
     // Creating new settings which include old ones and apply new ones over it
     if (inputSettings) {
       Object.assign(this.settings, inputSettings);
@@ -75,3 +82,4 @@ export class PixiButton extends NineSlicePlane {
     this.pivot.set(width * 0.5, height * 0.5);
   }
 }
+
--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,40 +1,39 @@
-import { ButtonContainer } from "@pixi/ui";
 import { Scene } from "../Scene";
 import { ReservedEntity } from "../entities";
 import { LoadingService } from "../services/LoadingService";
 import { SCREENX_PX, SCREENY_PX } from "../units/convert";
 import { QC } from "../components";
-import { PixiButton } from "../PixiButton";
-import { Container } from "pixi.js";
+import { Button, ButtonStyle } from "../guis/Button";
 import { hasLoadingCompleted } from "../components/LoadingState";
 import { SCENE_MANAGER, SceneId } from "../scenes";
 
 export default class MenuScene implements Scene {
-  button: PixiButton;
-  container: Container;
+  button: Button;
   constructor() {
     const appId = ReservedEntity.DEFAULT_PIXI_APP;
     const app = QC.getPixiApp(appId);
 
-    const button = (this.button = new PixiButton({ label: "start" }));
+    const button = (this.button = new Button(
+      new ButtonStyle({ label: "play" }),
+    ));
     button.x = SCREENX_PX / 2;
     button.y = SCREENY_PX / 2;
-    const container = (this.container = new ButtonContainer(button));
-    app.stage.addChild(container);
-    container.visible = false;
-    container.onPress.connect(this.startGame);
+    button.visible = false;
+    button.onPress.connect(this.playGame);
+    app.stage.addChild(button);
   }
   start() {
-    this.container.visible = true;
+    this.button.visible = true;
   }
-  startGame = () => {
-    this.container.parent!.removeChild(this.container);
+  playGame = () => {
+    const { button } = this;
+    button.parent!.removeChild(button);
     SCENE_MANAGER.start(SceneId.GAME);
   };
   update = () => {
     if (hasLoadingCompleted(ReservedEntity.GREEN_BUTTON_IMAGE)) {
       const texture = QC.getImage(ReservedEntity.GREEN_BUTTON_IMAGE).texture!;
-      this.button.texture = texture;
+      this.button.style.texture = texture;
     }
   };
   stop() {}
