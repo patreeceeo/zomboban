7200807d2b78c393efa88f8d72befa06c88e0fa7
Patrick Canfield <pscale01@gmail.com>
2024-01-18 21:21:40 -0800

    fix editor scene

--- src/scenes/EditorScene.ts
+++ src/scenes/EditorScene.ts
@@ -2,16 +2,25 @@ import { loadComponents } from "../Component";
 import { Scene } from "../Scene";
 import { COMPONENT_DATA_URL } from "../constants";
 import { LoadingService } from "../services/LoadingService";
-import { CameraSystem } from "../systems/CameraSystem";
+import { CameraSystem, initCameraSystem } from "../systems/CameraSystem";
 import { EditorSystem, stopEditorSystem } from "../systems/EditorSystem";
 import { EntityOperationSystem } from "../systems/EntityOperationSystem";
-import { RenderSystem } from "../systems/RenderSystem";
+import {
+  RenderSystem,
+  startRenderSystem,
+  stopRenderSystem,
+} from "../systems/RenderSystem";
 import { SwitchSceneSystem } from "../systems/SwitchSceneSystem";
 import "../components";
+import { getPixiApp } from "../components/PixiApp";
+import { ReservedEntity } from "../entities";
 
 export default class EditorScene implements Scene {
   start() {
     loadComponents(COMPONENT_DATA_URL);
+    initCameraSystem();
+    const app = getPixiApp(ReservedEntity.DEFAULT_PIXI_APP);
+    startRenderSystem(app);
   }
   update() {
     EditorSystem();
@@ -22,6 +31,8 @@ export default class EditorScene implements Scene {
   }
   stop() {
     stopEditorSystem();
+    const app = getPixiApp(ReservedEntity.DEFAULT_PIXI_APP);
+    stopRenderSystem(app);
   }
   services = [LoadingService];
 }
