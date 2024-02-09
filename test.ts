bf46a879b1fab6571c4179da6370461a2ef0f7ec
Patrick Canfield <pscale01@gmail.com>
2024-02-09 15:31:17 -0800

    simplify test runner

--- test.ts
+++ test.ts
@@ -1,11 +1,7 @@
 import { sync } from "glob";
 import _ from "lodash";
 
-const files = _.shuffle(sync("./src/**/*.test.ts")).filter(
-  (file) => !file.endsWith("Component.test.ts"),
-);
-// even with the await, the tests seem to run in parallel a bit. This seems to be the only test impacted by that so run it first.
-files.unshift("./src/Component.test.ts");
+const files = _.shuffle(sync("./src/**/*.test.ts"));
 for (const file of files) {
   await import(`./${file}`);
 }
