3f1ed7050d5623d75eccbc41ff2a36c3eb54af75
Patrick Canfield <pscale01@gmail.com>
2024-02-09 10:57:11 -0800

    rewrite Component

--- src/Component.test.ts
+++ src/Component.test.ts
@@ -1,71 +1,19 @@
 import test from "node:test";
-import {
-  appendComponentData,
-  getComponentData,
-  defineComponent,
-} from "./Component";
 import assert from "node:assert";
-import { executeFilterQuery } from "./Query";
-import { getNextEntityId, registerEntity } from "./Entity";
+import { PrimativeArrayComponent } from "./Component";
 
-await test("initComponentData", () => {
+test("PrimativeArrayComponent", () => {
   const NAME = "isVisible";
-  const getIsVisible = (entityId: number): boolean => DATA[entityId];
-  const setIsVisible = (entityId: number, value: boolean) => {
-    DATA[entityId] = value;
-  };
-  const hasIsVisible = (entityId: number): boolean =>
-    DATA[entityId] !== undefined;
-  const removeIsVisible = (entityId: number) => {
-    delete DATA[entityId];
-  };
-  const DATA = defineComponent(
-    NAME,
-    [],
-    hasIsVisible,
-    getIsVisible,
-    setIsVisible,
-    removeIsVisible,
-  );
-  const COMPONENT_DATA = getComponentData();
-
-  assert.equal(DATA, COMPONENT_DATA[NAME]);
-
-  // hot reload
-  DATA[0] = true;
-  assert.equal(
-    DATA,
-    defineComponent(
-      NAME,
-      DATA,
-      hasIsVisible,
-      getIsVisible,
-      setIsVisible,
-      removeIsVisible,
-    ),
-  );
-});
-
-function testAppend<T>(
-  source: T[],
-  data: T[],
-  nextEntityId: number,
-  expectedData: ReadonlyArray<T>,
-  expectedEntities: ReadonlyArray<number>,
-) {
-  appendComponentData(source, data, nextEntityId);
-
-  assert.deepEqual(data, expectedData);
-
-  for (let i = 0; i < data.length; i++) {
-    registerEntity(i);
-  }
-  const queryResult = executeFilterQuery((_id) => true, []);
-  assert.deepEqual(queryResult, expectedEntities);
-  assert.equal(getNextEntityId(), expectedEntities.length);
-}
-
-await test("appendComponentData", () => {
-  testAppend([7, 5, 3], [], 3, [, , , 7, 5, 3], [0, 1, 2, 3, 4, 5]);
-  testAppend([7, 5, 3], [], 3, [, , , 7, 5, 3], [0, 1, 2, 3, 4, 5]);
+  const DATA = [] as boolean[];
+  const Component = new PrimativeArrayComponent(NAME, DATA);
+  Component.addSet(42, true);
+  assert.equal(Component.get(42), true);
+  assert.equal(Component.has(42), true);
+  assert.equal(Component.has(0), false);
+  assert.throws(() => Component.get(0));
+  assert.equal(Component.get(0, true), true);
+  assert.equal(Component.serialize(42), true);
+  assert.throws(() => Component.serialize(0));
+  Component.deserialize(0, false);
+  assert.equal(Component.get(0), false);
 });
--- src/Component.ts
+++ src/Component.ts
@@ -1,219 +1,85 @@
-import { Counter } from "./Counter";
-import { registerEntity } from "./Entity";
 import { invariant } from "./Error";
-import { inflateString } from "./util";
 
-interface HasFunction {
-  (entityId: number): boolean;
-}
-interface GetFunction<T> {
-  (entityId: number): T;
-}
-interface AddSetFunction<T> {
-  (entityId: number, value: T): void;
-}
-interface RemoveFunction {
-  (entityId: number): void;
-}
+export abstract class ComponentBase<
+  Name extends String,
+  Item,
+  Collection,
+  SerializedItem = Item,
+> {
+  constructor(
+    readonly name: Name,
+    _data: Collection,
+  ) {}
 
-type Maybe<T> = T | undefined;
-export interface ComponentSerializer<I = unknown, O = unknown> {
-  (data: Maybe<I>[]): Maybe<O>[];
-}
+  isSaneGet(entityId: number, defaultValue?: Item): boolean {
+    return defaultValue !== undefined || this.has(entityId);
+  }
 
-export interface ComponentDeserializer<I = unknown, O = unknown> {
-  (data: Maybe<I>[], startIndex?: number): Promise<Maybe<O>[]>;
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept("./constants", () => {});
-}
-
-type ComponentName = string;
-type ComponentMap<T> = Record<ComponentName, T>;
-export type ComponentData = ComponentMap<unknown[]>;
-
-const COMPONENT_DATA: ComponentData = {};
-
-const HAS_COMPONENT: ComponentMap<HasFunction> = {};
-const GET_COMPONENT: ComponentMap<GetFunction<any>> = {};
-const ADD_SET_COMPONENT: ComponentMap<AddSetFunction<any>> = {};
-const REMOVE_COMPONENT: ComponentMap<RemoveFunction> = {};
-
-const SERIALIZERS: Record<ComponentName, ComponentSerializer<any>> = {};
-
-const DESERIALIZERS: Record<
-  ComponentName,
-  ComponentDeserializer<any> | undefined
-> = {};
-
-export function defineComponent<T>(
-  name: ComponentName,
-  data: T[],
-  has: HasFunction,
-  get: GetFunction<T>,
-  addSet: AddSetFunction<T>,
-  remove: RemoveFunction,
-  serialize = defaultComponentSerializer,
-  deserialize = defaultComponentDeserializer,
-): T[] {
-  COMPONENT_DATA[name] = data;
-  HAS_COMPONENT[name] = has;
-  GET_COMPONENT[name] = get;
-  ADD_SET_COMPONENT[name] = addSet;
-  REMOVE_COMPONENT[name] = remove;
-  SERIALIZERS[name] = serialize;
-  DESERIALIZERS[name] = deserialize;
-  return data;
+  abstract has(entityId: number): boolean;
+  abstract get(entityId: number, defaultValue?: Item): Item;
+  abstract addSet(entityId: number, value: Item): void;
+  abstract remove(entityId: number): void;
+  abstract serialize(entityId: number): SerializedItem;
+  abstract deserialize(
+    entityId: number,
+    value: SerializedItem,
+  ): void | Promise<void>;
 }
 
-export function getComponentData(): Readonly<ComponentData> {
-  return COMPONENT_DATA;
-}
+export abstract class ArrayComponentBase<
+  Name extends String,
+  Item,
+  SerializedItem = Item,
+> extends ComponentBase<Name, Item, Item[], SerializedItem> {
+  #data: Item[];
 
-export function listComponentNames(data: ComponentData) {
-  return Object.keys(data);
-}
+  constructor(name: Name, data: Item[]) {
+    super(name, data);
+    this.#data = data;
+    this.has = this.has.bind(this);
+    this.get = this.get.bind(this);
+    this.addSet = this.addSet.bind(this);
+    this.remove = this.remove.bind(this);
+  }
 
-// TODO fix this so it uses GET_COMPONENT but first need to rearchitect so that these are all pure functions
-export function getComponentPure<T = unknown>(
-  data: ComponentData,
-  name: ComponentName,
-  entityId: number,
-) {
-  return data[name][entityId] as T;
-}
+  has(entityId: number) {
+    return entityId in this.#data;
+  }
 
-export function appendComponentData<T>(
-  sourceData: T[],
-  targetData: T[],
-  nextEntityId: number,
-) {
-  for (
-    let i = 0, entityId = nextEntityId;
-    i < sourceData.length;
-    i++, entityId++
-  ) {
-    targetData[entityId] = sourceData[i];
-    registerEntity(entityId);
+  get(entityId: number, defaultValue?: Item) {
+    invariant(
+      this.isSaneGet(entityId, defaultValue),
+      `Entity ${entityId} has no ${this.name} component`,
+    );
+    return this.#data[entityId] ?? defaultValue!;
+  }
+
+  addSet(entityId: number, value: Item) {
+    invariant(
+      value !== undefined && value !== null,
+      `Value for ${this.name} component must be defined`,
+    );
+    this.#data[entityId] = value;
+  }
+
+  remove(entityId: number) {
+    delete this.#data[entityId];
   }
 }
 
-export function selectComponentData(
-  selectedComponents: readonly ComponentName[],
-  selectedEntities: readonly number[],
-) {
-  const componentData = getComponentData();
-  const selectedComponentData: ComponentData = {};
-  for (const componentName of selectedComponents) {
-    selectedComponentData[componentName] = [];
-    for (const entityId of selectedEntities) {
-      selectedComponentData[componentName][entityId] =
-        componentData[componentName][entityId];
-    }
+export class PrimativeArrayComponent<
+  Name extends string,
+  Item,
+> extends ArrayComponentBase<Name, Item> {
+  constructor(name: Name, data: Item[]) {
+    super(name, data);
   }
-  return selectedComponentData;
-}
 
-const defaultComponentSerializer: ComponentSerializer<any, any> = (data) =>
-  data;
-
-const defaultComponentDeserializer: ComponentDeserializer<any, any> = (data) =>
-  Promise.resolve(data);
-
-export function serializeComponentData(componentData: ComponentData): string {
-  return JSON.stringify(componentData, (key, value) => {
-    if (key in SERIALIZERS) {
-      const serializer = SERIALIZERS[key as ComponentName]!;
-      return serializer(value);
-    } else {
-      return value;
-    }
-  });
-}
-
-async function applyDeserializer<I, O>(
-  name: ComponentName,
-  data: Maybe<I>[],
-  nextEntityId: number,
-): Promise<Maybe<O>[]> {
-  const deserializer = DESERIALIZERS[name]!;
-  invariant(
-    deserializer !== undefined,
-    `No deserializer for ${name} component. Has its defining module been imported?`,
-  );
-  return (await deserializer(data, nextEntityId)) as Maybe<O>[];
-}
-
-let COMPONENT_RAW_DATA_CACHE: ArrayBuffer;
-
-async function fetchComponentRawData(
-  url: string,
-): Promise<ArrayBuffer | undefined> {
-  if (!COMPONENT_RAW_DATA_CACHE) {
-    const response = await fetch(url);
-    if (response.status === 200) {
-      COMPONENT_RAW_DATA_CACHE = await response.arrayBuffer();
-    } else if (!response.ok) {
-      console.error("Failed to fetch component data", response);
-    }
+  serialize(entityId: number) {
+    return this.get(entityId);
   }
-  return COMPONENT_RAW_DATA_CACHE!;
-}
 
-export const loadComponentsCursor = new Counter();
-
-let hasLoadingStarted = false;
-
-export async function loadComponents(url: string, forceReload = false) {
-  const nextEntityId = loadComponentsCursor.value;
-  if (forceReload || !hasLoadingStarted) {
-    hasLoadingStarted = true;
-    const rawData = await fetchComponentRawData(url);
-    if (!rawData) {
-      return;
-    }
-    const componentData = JSON.parse(
-      inflateString(new Uint8Array(rawData)),
-    ) as ComponentData;
-
-    for (const [name, value] of Object.entries(componentData)) {
-      const deserializedValue = await applyDeserializer(
-        name as ComponentName,
-        value,
-        nextEntityId,
-      );
-      appendComponentData(
-        deserializedValue,
-        COMPONENT_DATA[name as ComponentName],
-        nextEntityId,
-      );
-    }
+  deserialize(entityId: number, data: Item) {
+    this.addSet(entityId, data);
   }
-}
-
-export function removeComponentData(entityId: number) {
-  for (const name in COMPONENT_DATA) {
-    if (HAS_COMPONENT[name as ComponentName]?.(entityId)) {
-      REMOVE_COMPONENT[name as ComponentName]!(entityId);
-    }
-  }
-}
-
-export function hasComponentData(entityId: number) {
-  return Object.keys(COMPONENT_DATA).some((name) =>
-    HAS_COMPONENT[name as ComponentName]!(entityId),
-  );
-}
-
-export function getEntityComponentData(
-  entityId: number,
-): Record<ComponentName, any> {
-  const result: Record<ComponentName, any> = {};
-  for (const name in COMPONENT_DATA) {
-    if (HAS_COMPONENT[name as ComponentName]?.(entityId)) {
-      result[name] = GET_COMPONENT[name as ComponentName]!(entityId);
-    }
-  }
-  return result;
 }
