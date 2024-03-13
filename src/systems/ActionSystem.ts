5cfe16cc0a4d0d2edb6bf40a73097837b2b4a004
Patrick Canfield <pscale01@gmail.com>
2024-03-13 14:35:56 -0700

    update comment

--- src/systems/ActionSystem.ts
+++ src/systems/ActionSystem.ts
@@ -6,16 +6,17 @@ import { ActionsState } from "../state";
 
 /**
  * @fileoverview an application of the command pattern. I just like the word "action" better.
- * Every change to world state can be represented by an Action object, which is added to a queue to be applied.
+ *
+ * Motivation: support undo/redo.
+ *
  * Rules of Actions:
- *  - No other system should be able to mutate the game state (entity components) directly.
- *  - Once an action is added to the queue, it will be applied at the end of the frame.
- *  - Actions are immutable.
+ *  - All state mutation should be done through actions.
+ *  - Once an action is added to the queue, it will be applied in the current frame.
+ *  - Actions should be regarded as immutable.
  *  - The action does what it says. Keep it simple.
  *  - Avoid control flow statements (if, switch, for, while...) in actions.
  *     - Instead, make sure that only the appropriate actions are added to the queue.
  *
- * The motivation for this is to support undo/redo.
  *
  */
 
