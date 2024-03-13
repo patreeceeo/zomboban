8b2de83821c8c267ed2e1cd3f9e4d3675880d5e9
Patrick Canfield <pscale01@gmail.com>
2024-03-13 15:45:33 -0700

    "fix" max event listeners warning

--- test.ts
+++ test.ts
@@ -1,8 +1,11 @@
+import { setMaxListeners } from "events";
 import { sync } from "glob";
 import _ from "lodash";
 import { argv } from "process";
 
 const glob = argv[2] || "src/**/*.test.ts";
+
+setMaxListeners(55); // very scientific number /s
 
 const files = _.shuffle(sync(glob));
 for (const file of files) {
