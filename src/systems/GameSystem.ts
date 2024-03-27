a9493ee9c1949195df44b457d8a3262918544902
Patrick Canfield <pscale01@gmail.com>
2024-03-26 18:34:53 -0700

    fix initialization

--- src/systems/GameSystem.ts
+++ src/systems/GameSystem.ts
@@ -5,9 +5,8 @@ import { SystemWithQueries } from "../System";
 export class GameSystem extends SystemWithQueries<QueryState> {
   #gameEntities = this.createQuery([IsGameEntityTag]);
   start() {
-    for (const entity of this.#gameEntities) {
-      IsActiveTag.add(entity);
-    }
+    this.#gameEntities.stream((entity) => IsActiveTag.add(entity));
+    this.#gameEntities.onRemove((entity) => IsActiveTag.remove(entity));
   }
   stop() {
     for (const entity of this.#gameEntities!) {
