aabc8eacf22bb09a184f697283e5f366911cb963
Patrick Canfield <pscale01@gmail.com>
2024-04-02 20:47:08 -0700

    hide editor cursor

--- src/systems/GameSystem.ts
+++ src/systems/GameSystem.ts
@@ -1,12 +1,15 @@
 import { IsActiveTag, IsGameEntityTag } from "../components";
-import { QueryState } from "../state";
+import { EditorCursorState, QueryState } from "../state";
 import { SystemWithQueries } from "../System";
+
+type Context = QueryState & EditorCursorState;
 
 export class GameSystem extends SystemWithQueries<QueryState> {
   #gameEntities = this.createQuery([IsGameEntityTag]);
-  start() {
+  start(context: Context) {
     this.#gameEntities.stream((entity) => IsActiveTag.add(entity));
     this.#gameEntities.onRemove((entity) => IsActiveTag.remove(entity));
+    context.editorCursor.transform.visible = false;
   }
   stop() {
     for (const entity of this.#gameEntities!) {
