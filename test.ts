c4c47b87dbbde9179b7063c8aa0a8721cd937202
Patrick Canfield <pscale01@gmail.com>
2024-04-13 18:28:57 -0700

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,11 +1,8 @@
-import { setMaxListeners } from "events";
 import { sync } from "glob";
 import _ from "lodash";
 import { argv } from "process";
 
 const glob = argv[2] || "src/**/*.test.ts";
-
-setMaxListeners(55); // very scientific number /s
 
 const files = _.shuffle(sync(glob));
 for (const file of files) {
