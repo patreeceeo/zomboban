9e4bc9da96188f6c27a2ec4628314374890db55c
Patrick Canfield <pscale01@gmail.com>
2024-05-07 15:26:31 -0700

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+SESSION_NAME="zomboban"
+PROJECT_DIR="$HOME/Code/zomboban"
+
+EXISTING_INSTANCE=$(tmux list-clients -F \#S | grep "$SESSION_NAME")
+
+if [ -n "$EXISTING_INSTANCE" ]; then
+  tmux attach -t $SESSION_NAME
+  exit 0
+fi
+
+tmux new -s $SESSION_NAME -d
+
+tmux bind -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xclip -i -f -selection primary | xclip -i -selection clipboard"
+
+tmux split-window -h
+
+tmux select-pane -t 0
+tmux split-window -v
+tmux split-window -v
+
+tmux select-pane -t 0
+tmux send-keys "cd $PROJECT_DIR" C-m
+tmux send-keys "yarn test-dev" C-m
+
+tmux select-pane -t 1
+tmux send-keys "cd $PROJECT_DIR" C-m
+tmux send-keys "yarn dev" C-m
+
+tmux select-pane -t 2
+tmux send-keys "cd $PROJECT_DIR" C-m
+
+tmux select-pane -t 3
+tmux send-keys "cd $PROJECT_DIR" C-m
+tmux send-keys "nvim" C-m
+
+tmux attach -t $SESSION_NAME
