5237044d2bffa5c05640a7d7d21e8fbcb98f4745
Patrick Canfield <pscale01@gmail.com>
2024-05-18 22:14:05 -0700

    hide action viz

--- src/systems/ActionDebugSystem.ts
+++ src/systems/ActionDebugSystem.ts
@@ -37,6 +37,7 @@ const UIActionRound = (data: Action<ActionEntity<any>, any>[]) => {
 declare const pendingActionsElement: HTMLElement;
 declare const undoingActionsElement: HTMLElement;
 declare const completedActionsElement: HTMLElement;
+declare const showActionViz: HTMLInputElement;
 export class ActionDebugSystem extends System<ActionsState> {
   #pendingActions = new UIElementArray(pendingActionsElement, UIAction);
   #undoingActions = new UIElementArray(undoingActionsElement, UIAction);
@@ -45,10 +46,27 @@ export class ActionDebugSystem extends System<ActionsState> {
     UIActionRound,
     new UIElementArrayOptions(5, true)
   );
+  update() {
+    const enabled = showActionViz.checked;
+    if (enabled) {
+      pendingActionsElement.style.display = "initial";
+      undoingActionsElement.style.display = "initial";
+      completedActionsElement.style.display = "initial";
+    } else {
+      pendingActionsElement.style.display = "none";
+      undoingActionsElement.style.display = "none";
+      completedActionsElement.style.display = "none";
+    }
+  }
   start(state: ActionsState) {
     this.#pendingActions.subscribe(state.pendingActions);
     this.#undoingActions.subscribe(state.undoingActions);
     this.#completedActions.subscribe(state.completedActions);
+    this.update();
+
+    showActionViz.onchange = () => {
+      this.update();
+    };
   }
   stop() {
     this.#pendingActions.unsubscribe();
