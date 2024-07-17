f0332a7cbf3c316605dac335396f79acfe4e57fe
Patrick Canfield <pscale01@gmail.com>
2024-07-17 16:38:29 -0700

    use type for islands

--- src/islands.ts
+++ src/islands.ts
@@ -1,6 +1,9 @@
-export default {
+import type { IslandsByNameMap } from "xui";
+const islands: IslandsByNameMap = {
   "dev-tools": {
     templateHref: "src/ui/devtools.html",
     mount: "./ui/devtools"
   }
 };
+
+export default islands;
