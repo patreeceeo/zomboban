d1e63df41699db63839df1206f5045c5f1e97135
Patrick Canfield <pscale01@gmail.com>
2024-07-22 13:57:38 -0700

    prevent shadowing

--- src/Zui/AttributeDirective.ts
+++ src/Zui/AttributeDirective.ts
@@ -32,17 +32,21 @@ export abstract class AttributeDirective extends Base {
     state: any
   ) {
     const queryResults = this.query(el);
-    for (const el of queryResults) {
+    for (const nodeWithDirective of queryResults) {
       let scope = state;
-      if (el.parentNode) {
+      if (nodeWithDirective.parentNode) {
         /**
          * Controllers provide scope for the descendants of the top-level island element,
          * but not the top-level element itself. Said another way, the
          * scope for any element is determined by its anscestors, if they exists.
          */
-        scope = this.getScope(el.parentNode, controllerMap, state);
+        scope = this.getScope(
+          nodeWithDirective.parentNode,
+          controllerMap,
+          state
+        );
       }
-      this.update(el, scope);
+      this.update(nodeWithDirective, scope);
     }
   }
   abstract update(el: HTMLElement, scope?: any): void;
