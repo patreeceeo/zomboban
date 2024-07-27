9a90d5603bf2f7b551e3b73c45a686e49636ed54
Patrick Canfield <pscale01@gmail.com>
2024-07-26 18:47:02 -0700

    create my-img element

--- src/islands.ts
+++ src/islands.ts
@@ -2,6 +2,9 @@ import { createIslandMap } from "./Zui/Island";
 import { namedTemplates } from "./templates";
 
 const islands = createIslandMap(namedTemplates, {
+  async "my-img"() {
+    return (await import("./ui/my-img")).default;
+  },
   async "my-toolbar"() {
     return (await import("./ui/my-toolbar")).default;
   },
--- src/templates.ts
+++ src/templates.ts
@@ -1,4 +1,5 @@
 export const namedTemplates = {
+  "my-img": "my-img.html",
   "my-toolbar": "my-toolbar.html",
   "my-admin-toolbar": "my-admin-toolbar"
 };
