c355661512265d1f2b635d9388c6ab091ab8627c
Patrick Canfield <pscale01@gmail.com>
2024-08-18 07:51:02 -0700

    remove unused utils

--- src/util.ts
+++ src/util.ts
@@ -22,62 +22,6 @@ export function throttle<TArgs extends any[], TReturn extends any>(
   return _.throttle(callback, delay, options);
 }
 
-export function awaitDefaultExport<T>(
-  promise: Promise<{ default: T }>
-): () => Promise<T> {
-  return () => promise.then((m) => m.default);
-}
-
-export function nextTick(): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, 0));
-}
-
-export async function when(callback: () => boolean) {
-  while (!callback()) {
-    await nextTick();
-  }
-}
-
-export function emptyObject<T extends Record<string | number | symbol, any>>(
-  obj: T
-) {
-  for (const key in obj) {
-    delete obj[key];
-  }
-}
-
-export function pick<T extends Record<string, any>, K extends keyof T>(
-  obj: T,
-  keys: K[]
-) {
-  return _.pick(obj, keys);
-}
-
-/** function that takes any object and returns an object with setter traps for each property, recursively.
- */
-export function makeReadonlyRecursively<T extends Record<string, any>>(
-  obj: T
-): T {
-  const result = {} as T;
-  for (const key in obj) {
-    if (typeof obj[key] === "object") {
-      result[key] = makeReadonlyRecursively(obj[key]);
-    } else {
-      result[key] = obj[key];
-    }
-  }
-  return new Proxy(result, {
-    set() {
-      throw new Error("Cannot write to read-only object");
-    }
-  });
-}
-
-export type ReadonlyDeep<T> =
-  T extends Record<string, any>
-    ? Readonly<{ [Key in keyof T]: ReadonlyDeep<T[Key]> }>
-    : T;
-
 export function isNumber(value: any): value is number {
   return typeof value === "number" && !isNaN(value);
 }
@@ -94,29 +38,8 @@ export function joinPath(...parts: string[]) {
   return nonEmptyParts.join("/").replace(/\/+/g, "/");
 }
 
-export function once(
-  callback: () => void,
-  cleanup: (callback: () => void) => void
-) {
-  let called = false;
-  return () => {
-    if (!called) {
-      called = true;
-      callback();
-    } else {
-      cleanup(callback);
-    }
-  };
-}
-
 export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-const PI2 = Math.PI * 2;
-// keep angle between 0 and 2PI
-export function normalizeAngle(angle: number) {
-  return (angle + PI2) % PI2;
 }
 
 export function createTrapVector2() {
