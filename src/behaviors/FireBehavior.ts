b237bd61dea64f39abc4b42d63664cb479561361
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-05-16 20:00:48 -0700

    use action to despawn fire

--- src/behaviors/FireBehavior.ts
+++ src/behaviors/FireBehavior.ts
@@ -1,5 +1,6 @@
+import {Action} from "../Action";
 import { Message, MessageAnswer, sendMessage } from "../Message";
-import { CanDeleteTag } from "../components";
+import {DespawnAction} from "../actions";
 import FireEntity from "../entities/FireEntity";
 import { MoveMessage, HitByMonsterMessage } from "../messages";
 import {
@@ -21,6 +22,19 @@ type Entity = ReturnType<typeof FireEntity.create>;
 
 export class FireBehavior extends Behavior<Entity, BehaviorContext> {
   onUpdateEarly(_entity: Entity) {}
+  onUpdateLate(entity: Entity, context: TimeState) {
+    if (entity.actions.size !== 0) return; // EARLY RETURN!
+    const actions = [] as Action<any, any>[];
+    const { inbox } = entity;
+    // Determine whether to despawn
+    const messages = inbox.getAll(HitByMonsterMessage);
+    if(messages.size > 0) {
+      actions.push(
+        new DespawnAction(entity, context.time),
+      )
+    }
+    return actions;
+  }
   messageHandlers = {
     [MoveMessage.Into.type]: (
       entity: Entity,
@@ -35,8 +49,5 @@ export class FireBehavior extends Behavior<Entity, BehaviorContext> {
         )
       );
     },
-    [HitByMonsterMessage.type]: (entity: Entity, _context: BehaviorContext) => {
-      CanDeleteTag.add(entity);
-    }
   };
 }
