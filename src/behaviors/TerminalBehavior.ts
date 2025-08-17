46e98e97034e8c3f595caaa76f25e1ee17b9b5c9
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-08-17 14:25:37 -0700

    remove HMR

--- src/behaviors/TerminalBehavior.ts
+++ src/behaviors/TerminalBehavior.ts
@@ -4,7 +4,6 @@ import { Message, sendMessage} from "../Message";
 import { MoveMessage } from "../messages";
 import { EntityWithComponents } from "../Component";
 import { BehaviorComponent, TilePositionComponent } from "../components";
-import { getHMRSupport } from "../HMR";
 
 type Entity = EntityWithComponents<
   typeof BehaviorComponent | typeof TilePositionComponent
@@ -24,20 +23,6 @@ class TerminalBehavior extends Behavior<Entity, State> {
         context
       ).reduceResponses()
   };
-}
-
-if (import.meta.hot) {
-  let oldBehaviorCtor = TerminalBehavior;
-  const hmrSupport = getHMRSupport();
-  import.meta.hot.accept((newMod) => {
-    if (newMod !== undefined) {
-      const newBehaviorCtor = newMod.TerminalBehavior;
-
-      hmrSupport.state.replaceBehavior(oldBehaviorCtor, newBehaviorCtor);
-
-      oldBehaviorCtor = newBehaviorCtor;
-    }
-  });
 }
 
 export default TerminalBehavior;
--- src/ui/events.ts
+++ src/ui/events.ts
@@ -3,4 +3,3 @@ import { EventTypeHelper } from "Zui/EventType";
 export const signOutEvent = new EventTypeHelper("signOut");
 export const signInEvent = new EventTypeHelper("signIn");
 
-export const hmrReloadTemplateEvent = new EventTypeHelper("hmrReloadTemplate");
