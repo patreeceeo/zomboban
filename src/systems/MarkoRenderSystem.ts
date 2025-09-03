05e4c82092383d77dd77e016c72e251d00139200
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-09-03 11:29:32 -0700

    fix tests by lazy loading marko file

--- src/systems/MarkoRenderSystem.ts
+++ src/systems/MarkoRenderSystem.ts
@@ -1,18 +1,36 @@
 import { System } from "../System";
 import { State } from "../state";
-import EntityInspectorTemplate from '../marko/EntityInspector.marko';
 import {invariant} from "../Error";
 
 
 export class MarkoRenderSystem extends System<State> {
   private container: HTMLElement | null = null;
   private component: Marko.MountedTemplate<any> | null = null;
+  private template: any = null;
   
-  start() {
+  private async loadTemplate() {
+    if (process.env.NODE_ENV === 'test') {
+      // In test environment, use a mock
+      return {
+        mount: () => ({
+          update: () => {},
+          destroy: () => {}
+        })
+      };
+    } else {
+      // Dynamically import the Marko template in non-test environments
+      const module = await import('../marko/EntityInspector.marko');
+      return module.default;
+    }
+  }
+  
+  async start() {
+    this.template = await this.loadTemplate();
+    
     this.container = document.getElementById('entity-inspector-root');
     invariant(this.container !== null, "Container element not found.");
     
-    this.component = EntityInspectorTemplate.mount({}, this.container);
+    this.component = this.template.mount({}, this.container);
   }
   
   update(state: State) {
